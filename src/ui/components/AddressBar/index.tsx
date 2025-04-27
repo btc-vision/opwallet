849c04509910a50dcdc6da1a7422bda589961604
matbout <48333440+matbout@users.noreply.github.com>
2025-04-27 14:23:10 +0400

    Add 0x before public key

--- src/ui/components/AddressBar/index.tsx
+++ src/ui/components/AddressBar/index.tsx
@@ -3,15 +3,12 @@ import { copyToClipboard, shortAddress } from '@/ui/utils';
 import { CopyOutlined } from '@ant-design/icons';
 
 import { useTools } from '../ActionComponent';
-import { Row } from '../Row';
-import { Text } from '../Text';
 
 export function AddressBar() {
     const tools = useTools();
-    const publicKey = useAccountPublicKey();
+    const publicKey = '0x' + useAccountPublicKey();
     return (
         <>
-
             <div className="pubkey_row">
                 <div className="pubkey_col_1">
                     <div className="pubkey_holder">
@@ -39,24 +36,6 @@ export function AddressBar() {
                     </div>
                 </div>
             </div>
-
-            {/*
-            <Row
-                selfItemsCenter
-                itemsCenter
-                style={{
-                    padding: 5,
-                    borderRadius: 7,
-                    backgroundColor: 'rgba(240, 129, 51, 0.15)'
-                }}
-                onClick={async () => {
-                    await copyToClipboard(publicKey).then(() => {
-                        tools.toastSuccess('Copied');
-                    });
-                }}>
-                <Text text={`Public Key: ${shortAddress(publicKey)}`} style={{ color: '#f08133' }} preset="regular" />
-                <CopyOutlined style={{ color: '#f08133', fontSize: 14 }} />
-            </Row>*/}
         </>
     );
 }
