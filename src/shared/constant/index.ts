52ebc3c7b37c7838afe1707715a9af3212ecc0be
Predmet <175954199+impredmet@users.noreply.github.com>
2025-10-18 18:48:52 +0400

    Update contract addresses for moto and pill in DEFAULT_CHAINS_MAP

--- src/shared/constant/index.ts
+++ src/shared/constant/index.ts
@@ -281,10 +281,7 @@ export const DEFAULT_CHAINS_MAP: { [key in ChainType]?: TypeChain<key> } = {
         disable: false,
         showPrice: false,
         defaultExplorer: 'mempool-space',
-        contractAddresses: {
-            moto: '0x8de4d9ee47d56f719986302a05aece375c7e7d493ba986dc6a1eb5029eaf294c',
-            pill: '0x21770d319becc52154546ff93d0cb9b9728b251f202fdd772faf6de80c15ef04'
-        }
+        contractAddresses: {}
     },
     [ChainType.BITCOIN_TESTNET4]: {
         enum: ChainType.BITCOIN_TESTNET4,
@@ -316,8 +313,8 @@ export const DEFAULT_CHAINS_MAP: { [key in ChainType]?: TypeChain<key> } = {
         showPrice: false,
         defaultExplorer: 'mempool-space',
         contractAddresses: {
-            moto: '0xe9fd8cc8cf1b1ef869be84973c3ec49c115b81e046119edd14cad5746d57d065',
-            pill: '0x5605480e53318234d9a20a847a3edbd0830d957ee7e44d60009b0aa078250b94'
+            moto: '0x4baf2993659029a2c406846cec0c9acbc310e3d1d55c8d2344a2f235d879903d',
+            pill: '0x5288a80fb6e6f22443b965af7ed0f5bc8d6e8345e4dc80221b7dad9e126a90d2'
         }
     },
     [ChainType.BITCOIN_SIGNET]: {
